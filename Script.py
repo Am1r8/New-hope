b9fb9c00a19196bcb3508c4f8fbe01fd3fc51747
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-04-15 11:44:04 -0400

    recieve

--- Script.py
+++ Script.py
@@ -1,18 +1,27 @@
+import os
+from twilio.rest import Client as cs
 from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse as mr
+import creds
+
+account_sid = creds.account_sid
+auth_token = creds.auth_token
+
+client = cs(account_sid, auth_token)
+
+# client.messages.create(
+#     to = creds.phone_number,
+#     from_ = "+19032518533",
+#     body= "hello world!"
+# )
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
-
+    resp = mr()
+    resp.message("Hello! You said: ")
     return str(resp)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
