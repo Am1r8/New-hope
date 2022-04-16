752a294769252708da42f1887bd7eb0453f110bd
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-04-16 01:19:14 -0400

    new features

--- Script.py
+++ Script.py
@@ -1,13 +1,25 @@
 import os
 from twilio.rest import Client as cs
-from flask import Flask, request, redirect
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse as mr
-import creds
+import json
+# import creds
+from threading import Thread
+import test
+from textblob import TextBlob
 
-account_sid = creds.account_sid
-auth_token = creds.auth_token
 
+app = Flask(__name__)
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
 client = cs(account_sid, auth_token)
+
+app = Flask(__name__)
+# account_sid = creds.account_sid
+# auth_token = creds.auth_token
+
+# client = cs(account_sid, auth_token)
 
 # client.messages.create(
 #     to = creds.phone_number,
@@ -15,13 +27,26 @@ client = cs(account_sid, auth_token)
 #     body= "hello world!"
 # )
 
-app = Flask(__name__)
+k = ""
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def sms_reply():
-    resp = mr()
-    resp.message("Hello! You said: ")
-    return str(resp)
+    global k
+    response = mr()
+    body = request.values.get('Body', None)
+    if body == "I'm tired":
+        k = body
+        print(k)
+        res = TextBlob(k)
+        print(res.sentiment.polarity)
+    response.message("jjj")
+    return str(response)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def run():
+    app.run(host='0.0.0.0',port=9090)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    
+keep_alive()
